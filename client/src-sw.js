c5d1bae1ec2044a02735651793a46619453e6871
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-06-06 03:34:01 -0500

    code restructer

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,6 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -30,11 +31,15 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
+
     cacheName: 'asset-cache',
     plugins: [
+
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
+      
     ],
   })
+
 );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,9 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file. DONE
+// TODO: Add CSS loaders and babel to webpack. DONE
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,55 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({
+      new HtmlWebpackPlugin ({
         template: './index.html',
-        title: 'text editor cards'
+        title: 'jot-the-thought',
+        favicon: './favicon.ico'
       }),
-
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'src-sw.js'
       }),
-
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
+        name: 'Jot the Thought',
+        short_name: 'jtt',
+        description: 'text editor for smarties',
+        background_color: '#ffffff',
+        start_url:'/',
         publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join('assets','icons'),
           },
-        ],
+        ]
       }),
     ],
-
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
           },
-        },
-      ],
-    },
+            {
+              test: /\.m?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                },
+              }
+            }
+          ]
+        }
+    }
   };
-};
